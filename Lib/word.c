3203b073452739ec731002c06aa5988e78001d82
JiaqingXu <xzz12051224@gmail.com>
2015-08-25 15:02:39 +0800

    word_init

--- Lib/word.c
+++ Lib/word.c
@@ -77,7 +77,7 @@ TkWord *tkword_insert(char *p)
     if(tp==NULL)
     {
         length = strlen(p);
-        tp=(TkWord *)malloc(sizeof(TkWord)+length+1);
+        tp=(TkWord *)mallocz(sizeof(TkWord)+length+1);
         tp->next=tk_hashtable[keyno];
         tk_hashtable[keyno] = tp;
         Dynarray_add(&tktable,tp);
@@ -91,4 +91,76 @@ TkWord *tkword_insert(char *p)
         *s = (char)'\0';
     }
     return tp;
+}
+
+void * mallocz(int size)
+{
+    void * ptr;
+    ptr = malloc(size);
+    if(!ptr && size)
+        error("内存分配失败！！！\n");
+    memset(ptr,0,size);
+    return ptr;
+}
+
+void init_lex()
+{
+    TkWord * tp;
+    static TkWord keywords[]={
+    {TK_PLUS,       NULL,       "+",           NULL,    NULL},
+    {TK_MINUS,      NULL,       "-",            NULL,   NULL},
+    {TK_STAR,       NULL,       "*",            NULL,   NULL},
+    {TK_DEVIDE,     NULL,        "/",           NULL,   NULL},
+    {TK_MOD,        NULL,       "%",            NULL,   NULL},
+    {TK_EQ,         NULL,       "==",           NULL,   NULL},
+    {TK_NEQ,        NULL,       "!=",           NULL,   NULL},
+    {TK_LT,         NULL,        "<",           NULL,   NULL},
+    {TK_LEQ,        NULL,        "<=",          NULL,   NULL},
+    {TK_GT,         NULL,        ">",           NULL,   NULL},
+    {TK_GEQ,        NULL,        ">=",          NULL,   NULL},
+    {TK_ASSIGN,     NUL,         "=",           NULL,   NULL},
+    {TK_POINTSTO,   NUL          "->",          NULL,   NULL},
+    {TK_DOT,        NULL,        ".",          NULL,   NULL},
+    {TK_AND,        NULL,        "&",           NULL,   NULL},
+    {TK_OPENPA,     NULL,        "(",           NULL,   NULL},
+    {TK_CLOSEPA,    NULL;         ")",          NULL,   NULL},
+    {TK_OPENBR,     NULL;         "[",         NULL,   NULL},
+    {TK_CLOSEBR,    NULL,         "]",          NULL,   NULL},
+    {TK_BEGIN,      NULL,         "{",          NULL,   NULL},
+    {TK_END,        NULL,         "}",          NULL,   NULL},
+    {TK_SEMICOLON,  NULL,         ";",          NULL,   NULL},
+    {TK_COMMA,      NULL,          ",",         NULL,   NULL},
+    {TK_ELLIPSIS,   NULL,       "...",          NULL,   NULL},
+    {TK_EOF,        NULL,     "End_Of_File",    NULL,   NULL},
+
+    /*常量*/
+    {TK_CINT,       NULL,     "整型常量",       NULL,   NULL},
+    {TK_CCHAR,      NULL,     "字符常量",       NULL,   NULL},
+    {TK_CSTR,       NULL,      "字符串常量",    NULL,    NULL},
+
+    /*关键字*/
+    {KW_CHAR,       NULL,     "char",           NULL,   NULL},
+    {KW_SHORT,      NULL,     "short",          NULL,   NULL},
+    {KW_INT,        NULL,     "int",            NULL,   NULL},
+    {KW_VOID,       NULL,     "void",           NULL,   NULL},
+    {KW_STRUCT,     NULL,     "struct",         NULL,   NULL},
+    {KW_IF,         NULL,     "if",             NULL,   NULL},
+    {KW_ELSE,       NULL,     "else",           NULL,   NULL},
+    {KW_FOR,        NULL,     "for",            NULL,   NULL},
+    {KW_CONTINUE,   NULL,     "continue",       NULL,   NULL},
+    {KW_BREAK,      NULL,     "break",          NULL,   NULL},
+    {KW_RETURN,     NULL,     "return",         NULL,   NULL},
+    {KW_SIZEOF,     NULL,     "sizeof",         NULL,   NULL},
+
+    {KW_ALIGN,      NULL,     "__align",        NULL,   NULL},
+    {KW_CDECL,      NULL,     "__cdecl",        NULL,   NULL},
+    {KW_STDCALL,    NULL,     "__sedcall",      NULL,   NULL},
+
+    {0,             NULL,       NULL,           NULL,   NULL}
+    };
+    dynarray_init(&tktable,8);
+    for(tp=&keywords[0];tp->spelling!=NULL;tp++)
+    {
+        tkword_direct_insert(tp);
+    }
 }
